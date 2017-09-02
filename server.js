8301928ed470f7066a67fec24a37d170341adb6e
Tyler Jurecki <jurecki07@yahoo.com>
2017-09-02 15:53:33 -0500

    almost done

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const session = require("express-session");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 const path = require("path");
-// const sessionConfig = require("./sessionConfig");
-// const users = require("./data");
-// const checkAuth = require("./middlewares/checkAuth");
-// const indexRoutes = require("./routes/indexRoutes");
-// const authRoutes = require("./routes/authRoutes");
-// const userRoutes = require("./routes/userRoutes");
+const sessionConfig = require("./sessionConfig");
+const users = require("./data");
+const checkAuth = require("./middlewares/checkAuth");
+const indexRoutes = require("./routes/indexRoutes");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -26,9 +26,9 @@ app.use(logger("dev"));
 
 
 // //Routes
-// app.use("/", indexRoutes);
-// app.use("/auth", authRoutes); // /auth/signup
-// app.use("/user", checkAuth, userRoutes);
+app.use("/", indexRoutes);
+app.use("/auth", authRoutes); // /auth/signup
+app.use("/user", checkAuth, userRoutes);
 
 
 app.listen(port, () => {
